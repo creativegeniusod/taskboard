521251e8528403a0db583f135920e6cc3daac90b
sandeep-odz <sandeep@ourdesignz.in>
2019-12-26 13:42:18 +0530

    changes in app.component.html aand app.component.css

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import * as moment from "moment";
-import { TaskService } from "./services/task.service";
+import * as moment from 'moment';
+import { TaskService } from './services/task.service';
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   taskForm: FormGroup;
-  minDate: any = "";
+  minDate: any = '';
   tasks: any = [];
   constructor(
     private formBuilder: FormBuilder,
@@ -19,12 +19,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.taskForm = this.formBuilder.group({
-      title: ["", [Validators.required]],
-      taskDate: [moment().format("YYYY-MM-DD"), Validators.required],
-      description: ["", [Validators.maxLength(250)]]
+      title: ['', [Validators.required]],
+      taskDate: [moment().format('YYYY-MM-DD'), Validators.required],
+      description: ['', [Validators.maxLength(250)]]
     });
 
-    this.minDate = moment().format("YYYY-MM-DD");
+    this.minDate = moment().format('YYYY-MM-DD');
 
     this.taskService.getTasks().subscribe(
       (res: any) => {
@@ -35,29 +35,24 @@ export class AppComponent implements OnInit {
     );
   }
 
-  // /**
-  //  * Use to check foem errors and return if any
-  //  *
-  //  * @param String controlName
-  //  * @param String errorName
-  //  */
-  // hasError = (controlName: string, errorName: string) => {
-  //   this.taskForm.controls[controlName].hasError(errorName);
-  // };
-
   get formControl() {
     return this.taskForm.controls;
   }
 
   submitForm() {
     //console.log(this.taskForm.value);
+    if (this.taskForm.value.title.trim() == '') {
+      this.taskForm.controls['title'].setErrors({ required: true });
+      this.taskForm.patchValue({ title: '' });
+      return;
+    }
     this.taskService.createTask(this.taskForm.value).subscribe(
       (res: any) => {
         //console.log(res);
         this.tasks = res.data;
         this.taskForm.reset();
         this.taskForm.patchValue({
-          taskDate: moment().format("YYYY-MM-DD")
+          taskDate: moment().format('YYYY-MM-DD')
         });
       },
       err => {}
@@ -76,9 +71,7 @@ export class AppComponent implements OnInit {
     );
   }
 
-  trunc(a) {
-    return a.slice(0, 251) + "...";
-  }
-
-  //nums.sort(compare);
+  // trunc(a) {
+  //   return a.slice(0, 251) + "...";
+  // }
 }
